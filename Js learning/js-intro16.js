1a57ccc5458a2ed346c722183195627614d24eca
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2021-12-03 16:52:12 +0530

    c

--- Js learning/js-intro16.js
+++ Js learning/js-intro16.js
@@ -1,26 +1,45 @@
-// problems in asynchronous programming 
-// console.log("output 1");
+// // problems in asynchronous programming 
+// console.log("output 1"); // 1
 // const getFun = () => {
 //     setTimeout(() => {
 //         return { message: "fun meesage" }; // react to spring boot 
 //     }, 2000);
 // }
 // // TypeError: Cannot read property 'message' of undefined
-// const fun = getFun();
-// console.log(fun.message); //  depends on output  2
+// const fun = getFun(); // 2, but delayed 
+// console.log(fun.message); //  3, fails because it depends on output  2
 
 // solution 1. - use callback 
+// console.log("output 1"); // 1 
 
-console.log("output 1");
-const getFun = (abc) => {
-    setTimeout(() => {
-        abc({ message: "fun meesage" }); // callback 
-    }, 2000);
+// const getFun = (abc) => {  // 3
+//     setTimeout(() => {
+//         abc({ message: "fun meesage" }); // callback  // 4
+//     }, 2000);
+// }
+// const funCall = (arg) => { // 5 
+//     console.log(arg.message);  // 6
+// }
+// // success 
+// getFun(funCall); // 2 
+
+// solution 2 -  use Promise  
+console.log("output 1"); // 1 
+
+const getFun = () => { // 3
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const err = false; // true will rejects/ false will print 
+            if (err)
+                reject();
+            else
+                resolve({ message: "fun meesage" });
+        }, 2000);
+    });
 }
 
-const funCall = (arg) => {
-    console.log(arg.message);
-}
-
-// success 
-getFun(funCall);
+getFun().then((obj) => {
+    console.log(obj.message);
+}).catch(() => {
+    console.log("Error...");
+});
